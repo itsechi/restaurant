85a598f928141e70bdb381ed4732f7e120dae2cd
itsechi <wardulinskao@gmail.com>
2022-08-15 19:57:27 +0200

    Install and configure webpack

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  devtool: false,
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: "src/template.html",
+    }),
+  ],
+};
