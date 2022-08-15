660f4e0cab5d8b7438b813f9b5208bb293bb686b
itsechi <wardulinskao@gmail.com>
2022-08-15 20:41:15 +0200

    Add a function to create nav and header

--- main.js
+++ main.js
@@ -1,8 +1,162 @@
 /******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/modules/logo.js":
+/*!*****************************!*\
+  !*** ./src/modules/logo.js ***!
+  \*****************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (/* binding */ createLogo)
+/* harmony export */ });
+function createLogo() {
+  const content = document.getElementById('content');
+
+  const logoContainer = document.createElement('header');
+  const logoRestaurant = document.createElement('h1');
+  logoRestaurant.textContent = 'Restaurant';
+
+  const logoName = document.createElement('h1');
+  logoName.textContent = 'PURE COVE';
+
+  logoContainer.appendChild(logoRestaurant);
+  logoContainer.appendChild(logoName);
+  content.appendChild(logoContainer);
+}
+
+/***/ }),
+
+/***/ "./src/modules/nav.js":
+/*!****************************!*\
+  !*** ./src/modules/nav.js ***!
+  \****************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (/* binding */ createNav)
+/* harmony export */ });
+function createNav() {
+  const content = document.getElementById('content');
+  const nav = document.createElement('nav');
+
+  const home = document.createElement('button');
+  home.textContent = 'HOME';
+  home.classList.add('home');
+
+  const menu = document.createElement('button');
+  menu.textContent = 'MENU';
+  menu.classList.add('menu');
+
+  const contact = document.createElement('button');
+  contact.textContent = 'CONTACT';
+  contact.classList.add('contact');
+
+  nav.appendChild(home);
+  nav.appendChild(menu);
+  nav.appendChild(contact);
+  content.appendChild(nav);
+}
+
+
+
+/***/ }),
+
+/***/ "./src/pages/home.js":
+/*!***************************!*\
+  !*** ./src/pages/home.js ***!
+  \***************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (/* binding */ createHome)
+/* harmony export */ });
+/* harmony import */ var _modules_nav__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../modules/nav */ "./src/modules/nav.js");
+/* harmony import */ var _modules_logo__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../modules/logo */ "./src/modules/logo.js");
+
+
+
+
+function createHome() {
+  (0,_modules_nav__WEBPACK_IMPORTED_MODULE_0__["default"])();
+  (0,_modules_logo__WEBPACK_IMPORTED_MODULE_1__["default"])();
+}
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
 var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
+(() => {
 /*!**********************!*\
   !*** ./src/index.js ***!
   \**********************/
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _pages_home__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./pages/home */ "./src/pages/home.js");
+
+
+(0,_pages_home__WEBPACK_IMPORTED_MODULE_0__["default"])();
+
+})();
 
 /******/ })()
 ;
