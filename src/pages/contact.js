43f3d6387127c6d409ba29acbc74d76130cbb433
itsechi <wardulinskao@gmail.com>
2022-08-16 22:35:24 +0200

    Add contact sections

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -7,11 +7,51 @@ export default function createContact() {
   createLogo('medium');
 
   const content = document.getElementById('content');
+
+  const main = document.createElement('main');
+  main.classList.add('contact-main');
+  content.appendChild(main);
+
+  /* contact section */
+  const contactSection = document.createElement('section');
+  contactSection.classList.add('contact-section');
+  main.appendChild(contactSection);
+  const contactHeader = document.createElement('h2');
+  contactHeader.textContent = 'CONTACT US';
+  const contactNumber = document.createElement('p');
+  contactNumber.textContent = '+1 582-201-7138';
+  const contactEmail = document.createElement('p');
+  contactEmail.textContent = 'purecove@restaurant.com';
+  const contactBtn = document.createElement('button');
+  contactBtn.textContent = 'CONTACT FORM';
+
+  contactSection.appendChild(contactHeader);
+  contactSection.appendChild(contactNumber);
+  contactSection.appendChild(contactEmail);
+  contactSection.appendChild(contactBtn);
+
+  /* map section */
   const map = document.createElement('img');
   map.src = mapImg;
-  map.alt = 'location of the restaurant'
+  map.alt = 'location of the restaurant';
   map.classList.add('map');
-  content.appendChild(map);
+  main.appendChild(map);
+
+  /* address section */
+  const addressSection = document.createElement('section');
+  addressSection.classList.add('contact-section');
+  main.appendChild(addressSection);
+  const addressHeader = document.createElement('h2');
+  addressHeader.textContent = 'ADDRESS';
+  const address1 = document.createElement('p');
+  address1.textContent = '85 South 9th Street';
+  const address2 = document.createElement('p');
+  address2.textContent = 'Brooklyn, NY';
+  const addressBtn = document.createElement('button');
+  addressBtn.textContent = 'BOOK A TABLE';
+
+  addressSection.appendChild(addressHeader);
+  addressSection.appendChild(address1);
+  addressSection.appendChild(address2);
+  addressSection.appendChild(addressBtn);
 }
-
-
